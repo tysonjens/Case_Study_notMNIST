44cb02c9f00b670d7794aba911eb53f96fc27188
Tyson Ward <tysonjens@gmail.com>
2018-04-13 13:14:29 -0600

    add models

---
+++ mod2_notMNIST.py
@@ -0,0 +1,146 @@
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import regularizers
+import matplotlib.pyplot as plt
+import numpy as np
+from keras import backend as K
+import os
+import skimage.io
+from IPython.display import clear_output
+
+class PlotLearning(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.i = 0
+        self.x = []
+        self.losses = []
+        self.val_losses = []
+        self.acc = []
+        self.val_acc = []
+        self.fig = plt.figure()
+
+        self.logs = []
+
+    def on_epoch_end(self, epoch, logs={}):
+
+        self.logs.append(logs)
+        self.x.append(self.i)
+        self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
+        self.acc.append(logs.get('acc'))
+        self.val_acc.append(logs.get('val_acc'))
+        self.i += 1
+        f, (ax1, ax2) = plt.subplots(1, 2, sharex=True)
+
+        clear_output(wait=True)
+
+        ax1.set_yscale('log')
+        ax1.plot(self.x, self.losses, label="loss")
+        ax1.plot(self.x, self.val_losses, label="val_loss")
+        ax1.legend()
+
+        ax2.plot(self.x, self.acc, label="accuracy")
+        ax2.plot(self.x, self.val_acc, label="validation accuracy")
+        ax2.legend()
+
+        plt.show();
+
+plot = PlotLearning()
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, split between train and test sets
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+letter = ['A','B','C','D','E','F','G','H','I','J']
+x_train = []
+y_train = []
+x_test = []
+y_test = []
+for i, l in enumerate(letter):
+    count_letter = 0
+    folder = 'data/notMNIST_small/' + l
+    files = os.listdir(folder)
+    for f in files:
+        path_img = folder + '/' + f
+        try:
+            img = skimage.io.imread(str(path_img))
+        except:
+            continue
+        count_letter += 1
+        if count_letter % 7 ==0:
+            x_test.append(img)
+            y_test.append(i)
+        else:
+            x_train.append(img)
+            y_train.append(i)
+
+x_train = np.array(x_train)
+x_test = np.array(x_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+## 1
+model.add(Conv2D(16, kernel_size=(4, 4),
+                 activation='relu',padding='same',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+## 2
+model.add(Conv2D(32, (4, 4), activation='relu', padding='same'))
+## 3
+model.add(Conv2D(64, (4, 4), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+## 4
+model.add(Conv2D(64, (4, 4), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+## 5
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(1024, activation='relu', kernel_regularizer=regularizers.l2(0.)))
+model.add(Dropout(0.5))
+model.add(Dense(205, activation='elu', kernel_regularizer=regularizers.l2(0.)))
+# model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test), callbacks=[plot])
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
